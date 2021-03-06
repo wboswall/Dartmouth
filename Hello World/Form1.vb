310d7cda992bec9d7f805dfb59be05dd4ebde563
William Boswall <wboswall@live.com>
2021-03-06 17:32:02 -0400

    Commit Hello World
    Application cleaned and released.

---
+++ Hello World/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Label1.Text = "Hello World!"
+	End Sub
+
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+		Me.Close()
+	End Sub
+End Class
